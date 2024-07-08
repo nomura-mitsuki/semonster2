97593e7cc3350ddd72edb74acbc03182b28b426f
nomura <e1q21079@oit.ac.jp>
2024-07-08 12:04:47 +0900

    MonsterTestデバック

--- app/src/main/java/org/example/Monster.java
+++ app/src/main/java/org/example/Monster.java
@@ -1,5 +1,7 @@
+package org.example;
+
 public class Monster {
-    private String name;
+  private String name;
   private int rare; // 戦うとレア度が高いほうが勝つ．同じ場合は引き分け
 
   Monster(int nameNum, int rareNum) {
@@ -10,10 +12,10 @@ public class Monster {
   String summonMonster(int mnumber) {
     String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
     String monstersEvol[] = { "メタルスライム", "スーパーサハギン", "ホワイトドラゴン", "ダークデュラハン", "ブラックシーサーペント" };
-    if (this.rare >= 3{
-        return monstersEvol[mnumber];
-    }else{
-        return monsters[mnumber];
+    if (this.rare >= 3) {
+      return monstersEvol[mnumber];
+    } else {
+      return monsters[mnumber];
     }
   }
 
