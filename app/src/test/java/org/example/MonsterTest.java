eac5de03a1bdcb218c7035275aeb87595ffb6a2a
野村美月 <e1q21079@oit.ac.jp>
2024-07-08 11:24:29 +0900

    MonsterTest.java

--- app/src/test/java/org/example/MonsterTest.java
+++ app/src/test/java/org/example/MonsterTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 public class MonsterTest {
-
+　
     @Test
     public void testSummonMonster() {
         Monster monster = new Monster(0, 1);
