c877eb9547bd9e49364c9f86617711996844c582
野村美月 <e1q21079@oit.ac.jp>
2024-07-08 11:22:08 +0900

    MonsterTest.java(野村)

--- app/src/test/java/org/example/MonsterTest.java
+++ app/src/test/java/org/example/MonsterTest.java
@@ -5,14 +5,33 @@ package org.example;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MonsterTest {
-  @Test
-  public void testSummonMonster() {
-    Monster monster = new Monster();
-    assertEquals(5, calculate.sum_loop(2, 3, 0)); // 2 + 3
-    assertEquals(55, calculate.sum_loop(1, 10, 0)); // 1 + 2 + ... + 10
-    assertEquals(25, calculate.sum_loop(1, 10, 1)); // 1 + 3 + 5 + 7 + 9 (odd)
-    assertEquals(30, calculate.sum_loop(1, 10, 2)); // 2 + 4 + 6 + 8 + 10 (even)
-  }
+
+    @Test
+    public void testSummonMonster() {
+        Monster monster = new Monster(0, 1);
+        assertEquals("スライム", monster.summonMonster(0));
+        assertEquals("サハギン", monster.summonMonster(1));
+        assertEquals("ドラゴン", monster.summonMonster(2));
+        assertEquals("デュラハン", monster.summonMonster(3));
+        assertEquals("シーサーペント", monster.summonMonster(4));
+    }
+
+    @Test
+    public void testToString() {
+        Monster monster1 = new Monster(2, 5);
+        assertEquals("ドラゴン:レア度[5]\n", monster1.toString());
+
+        Monster monster2 = new Monster(4, 3);
+        assertEquals("シーサーペント:レア度[3]\n", monster2.toString());
+    }
+
+    @Test
+    public void testMonsterConstructor() {
+        Monster monster = new Monster(1, 10);
+        assertEquals("サハギン", monster.summonMonster(1));
+        assertEquals(10, monster.rare);
+    }
 }
